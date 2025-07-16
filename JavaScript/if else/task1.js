8a318f4822476f9ee51f88c7009728f356634c2f
SriHaran23 <sriharankandepalli@gmail.com>
2025-07-16 14:34:05 +0530

    changes

---
+++ JavaScript/if else/task1.js
@@ -0,0 +1,12 @@
+var a=10, b=20, c=30;
+            if(a>b && a>c){
+                console.log('a is bigger');
+            }
+            else if(b>a && b>c){
+                console.log('b is bigger');
+
+            }
+            else{
+                console.log('c is bigger');
+
+            }
